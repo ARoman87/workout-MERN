a59678decb3ffe17b2ab6fc7193ba28410ed32f8
ARoman87 <angel427roman@gmail.com>
2023-01-12 16:06:33 -0500

    All Workout components completed

--- client/src/components/WorkoutDetails/WorkoutDetails.jsx
+++ client/src/components/WorkoutDetails/WorkoutDetails.jsx
@@ -1,12 +1,31 @@
 import "./WorkoutDetails.css"
+import { useWorkoutsContext } from "../../hooks/useWorkoutContext"
+
+// Date fns
+import formatDistanceToNow from "date-fns/formatDistanceToNow"
 
 export default function WorkoutDetails(props) {
+
+    const {dispatch} = useWorkoutsContext()
+
+    const handleClick = async () => {
+        const response = await fetch("http://localhost:4000/api/workouts/" + props.workout._id, {
+            method: "DELETE"
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({type: "DELETE_WORKOUT", payload: json})
+        }
+    }
+
     return (
         <div className="workout-details">
             <h4>{props.workout.title}</h4>
             <p><strong>Load (lbs):</strong> {props.workout.load}</p>
             <p><strong>Reps:</strong> {props.workout.reps}</p>
-            <p>{props.workout.createdAt}</p>
+            <p>{formatDistanceToNow(new Date(props.workout.createdAt), {addSuffix: true})}</p>
+            <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
         </div>
     )
 }
--- client/src/components/WorkoutForm/WorkoutForm.jsx
+++ client/src/components/WorkoutForm/WorkoutForm.jsx
@@ -1,11 +1,18 @@
 import {useState} from "react"
 import "./WorkoutForm.css"
-export default function WorkoutForm() {
+import { useWorkoutsContext } from "../../hooks/useWorkoutContext"
 
+
+
+
+
+export default function WorkoutForm() {
+    const {dispatch} = useWorkoutsContext()
     const [title, setTitle] = useState("")
     const [load, setLoad] = useState("")
     const [reps, setReps] = useState("")
     const [error, setError] = useState(null)
+    const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -23,13 +30,15 @@ export default function WorkoutForm() {
     
         if (!response.ok) {
           setError(json.error)
+          setEmptyFields(json.emptyFields)
         }
         if (response.ok) {
           setError(null)
           setTitle('')
           setLoad('')
           setReps('')
-          console.log('new workout added:', json)
+          setEmptyFields([])
+          dispatch({type: "CREATE_WORKOUT", payload: json})
         }
     
       }
@@ -44,6 +53,7 @@ export default function WorkoutForm() {
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes("title") ? "error" : ""}
             />
 
             <label>Load (in lbs):</label>
@@ -51,6 +61,7 @@ export default function WorkoutForm() {
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes("load") ? "error" : ""}
             /> 
 
             <label>Reps:</label>
@@ -58,6 +69,7 @@ export default function WorkoutForm() {
                 type="number"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes("reps") ? "error" : ""}
             /> 
 
             <button>Add Workout</button>

44912ab7036ca84c879994e5931b01b17acb4589
ARoman87 <angel427roman@gmail.com>
2023-01-13 17:15:49 -0500

    Started Sigup and Login backend logic

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './App.css'
 import { WorkoutContextProvider } from './context/WorkoutContext'
+import { AuthContextProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AuthContextProvider>
     <WorkoutContextProvider>
       <App />
     </WorkoutContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )

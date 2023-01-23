21ec3d2492effc145c3db3230fa0cbe3d8298892
ARoman87 <angel427roman@gmail.com>
2023-01-23 17:35:05 -0500

    Ready to deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,9 @@ import App from './App'
 import './App.css'
 import { WorkoutContextProvider } from './context/WorkoutContext'
 import { AuthContextProvider } from './context/AuthContext'
+import {disableReactDevTools} from "@fvilers/disable-react-devtools"
+
+if (process.env.NODE_ENV === "production") disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

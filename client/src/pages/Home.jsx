4125182a326bf400764c8cd1c2b955fd9f7b0115
ARoman87 <angel427roman@gmail.com>
2023-01-23 19:43:41 -0500

    Fixed fetch address

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch("https://workout-log.onrender.com/api/workouts", {
+            const response = await fetch("https://workout-log-api.onrender.com/api/workouts", {
                 headers: {
                     "Authorization": `Bearer ${user.token}`
                 }

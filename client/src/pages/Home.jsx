5340246ae15c286bd628b43009f090edc4241236
ARoman87 <angel427roman@gmail.com>
2023-01-23 19:41:08 -0500

    Fixed styling issues and connected backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch("http://localhost:4000/api/workouts", {
+            const response = await fetch("https://workout-log.onrender.com/api/workouts", {
                 headers: {
                     "Authorization": `Bearer ${user.token}`
                 }
